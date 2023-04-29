3487058d1af6042fcb6661d551b692514a5fc15b
samira garcia <samira.garcia95@gmail.com>
2023-04-29 17:42:47 -0300

    terminado

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const INGREDIENTES_SOPA = "agua,verduras,carne o pollo";
+const inputIngrediente1 = document.querySelector("#ingrediente-1");
+const inputIngrediente2 = document.querySelector("#ingrediente-2");
+const inputIngrediente3 = document.querySelector("#ingrediente-3");
+const inputIngrediente4 = document.querySelector("#ingrediente-4");
+const inputIngrediente5 = document.querySelector("#ingrediente-5");
+const inputIngrediente6 = document.querySelector("#ingrediente-6");
 
-var ingredienteCondimento = confirm ("hay condimentos para la sopa?");
-var ingredienteCuboCaldo = confirm ("hay un cubo de caldo para la sopa?");
+const pResultado = document.querySelector("#resultado");
 
-if (INGREDIENTES_SOPA && ingredienteCondimento) {
-    alert ("el robot realizo la sopa")
-} else { 
-    alert ("la sopa no se realizo por falta de ingredientes")
+function verificarPreparacionSopa(){
+    let tenemosIngrediente1 =inputIngrediente1.value;
+    let tenemosIngrediente2 =inputIngrediente2.value;
+    let tenemosIngrediente3 =inputIngrediente3.value;
+    let tenemosIngrediente4 =inputIngrediente4.value;
+    let tenemosIngrediente5 =inputIngrediente5.value;
+    let tenemosIngrediente6 =inputIngrediente6.value;
+    console.log(tenemosIngrediente1);
+    console.log(tenemosIngrediente2);
+    console.log(tenemosIngrediente3);
+    console.log(tenemosIngrediente4);
+    console.log(tenemosIngrediente5);
+    console.log(tenemosIngrediente6);
+    if(  ((tenemosIngrediente1 == "Y" && tenemosIngrediente2 == "Y") && (tenemosIngrediente3 == "Y" || tenemosIngrediente4 == "Y")) && (tenemosIngrediente5 == "Y" || tenemosIngrediente6 == "Y")){
+        pResultado.innerHTML = "Preparando sopa!";
+    }else{
+        pResultado.innerHTML = "No hay ingredientes necesarios para preparar la sopa!";
+    
+    }
 }
